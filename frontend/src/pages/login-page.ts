aaa07000f893d0b1b699a20becaecd1ee9275e36
FlorenceSiiWR <104396431@students.swinburne.edu.my>
2026-04-10 11:49:33 +0800

    - added login page

--- frontend/src/pages/login-page.ts
+++ frontend/src/pages/login-page.ts
@@ -1,17 +1,45 @@
 import { LitElement, html } from 'lit';
 import { Router } from '@vaadin/router';
+import { customElement, state } from 'lit/decorators.js';
 
-class LoginPage extends LitElement {
+@customElement('login-page')
+export class LoginPage extends LitElement {
+
+  @state()
+  private username: string = '';
+
+  @state()
+  private password: string = '';
 
   handleLogin() {
+    if (!this.username || !this.password) {
+      alert('Please enter username and password');
+      return;
+    }
+
     Router.go('/chat');
   }
 
   render() {
     return html`
+    <div class="login">
       <h2>Login</h2>
-      <input placeholder="Username">
-      <button @click=${this.handleLogin}>Login</button>
+      <p>Username</p>
+      <input
+        type="text"
+        placeholder="Username"
+        @input=${(e: any) => this.username = e.target.value}
+      />
+      <br>
+
+      <p>Password</p>
+      <input
+        type="password"
+        placeholder="Password"
+        @input=${(e: any) => this.password = e.target.value}
+      />
+    </div>
+        <button @click=${this.handleLogin}>Login</button>
 
       <p>
         No account?
@@ -20,5 +48,3 @@ class LoginPage extends LitElement {
     `;
   }
 }
-
-customElements.define('login-page', LoginPage);
