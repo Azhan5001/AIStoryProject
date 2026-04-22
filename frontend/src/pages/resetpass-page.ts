0e40d547c1758574704f3c1fde589d3d2805b3c3
FlorenceSiiWR <104396431@students.swinburne.edu.my>
2026-04-22 18:57:14 +0800

    -updated darkmode testing

--- frontend/src/pages/resetpass-page.ts
+++ frontend/src/pages/resetpass-page.ts
@@ -1,23 +1,38 @@
 import { LitElement, html, css } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 import '../components/reset-password';
-import styles from '../styles/login-page.css?inline';
+import styles from '../styles/resetpass-page.css?inline';
 import { unsafeCSS } from 'lit';
 
 @customElement('reset-password-page')
 export class ResetPasswordPage extends LitElement {
 
-  static styles = css`
-  ${unsafeCSS(styles)}
-`;
+  static styles = css`${unsafeCSS(styles)}`;
+
+  @state() darkMode = false;
+
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+
+    this.setAttribute(
+      'theme',
+      this.darkMode ? 'dark' : 'light'
+    );
+  }
 
   render() {
     return html`
-      <div class="login-background">
-      <div class="container">
-        <reset-password></reset-password>
+      <div class="background">
+
+        <button class="theme-toggle" @click=${this.toggleTheme}>
+          ${this.darkMode ? '☀️' : '🌙'}
+        </button>
+
+        <div class="container">
+          <reset-password></reset-password>
         </div>
-    </div>
+
+      </div>
     `;
   }
 }
